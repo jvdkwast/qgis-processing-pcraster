34938651039a4b2ca35fd125f614cef1bc38fe01
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 17:52:11 +0100

    Update pcraster_areanormal_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_areanormal_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_areanormal_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterAreanormalAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Value assigned to an area taken from a normal distribution
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_areanormal.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_areanormal.html">PCRaster documentation</a>
 
             Parameters:
 
