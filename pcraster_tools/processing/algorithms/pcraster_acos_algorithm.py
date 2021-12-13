2721a181e241ed786d5ce733df5ec1f392863def
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 17:49:50 +0100

    Update pcraster_acos_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_acos_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_acos_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterAcosAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Inverse cosine
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_acos.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_acos.html">PCRaster documentation</a>
 
             Parameters:
 
