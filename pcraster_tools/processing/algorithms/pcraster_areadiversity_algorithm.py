94381e64a9c432eb72f8a11508e8d0f5bd859be0
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 17:50:53 +0100

    Update pcraster_areadiversity_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_areadiversity_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_areadiversity_algorithm.py
@@ -46,7 +46,7 @@ class PCRasterAreadiversityAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Number of unique cell values within an area
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_areadiversity.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_areadiversity.html">PCRaster documentation</a>
 
             Parameters:
 
