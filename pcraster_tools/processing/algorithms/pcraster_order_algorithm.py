faa65d633da0c0a6723172817d498933527bf2ad
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:07:13 +0100

    Update pcraster_order_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_order_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_order_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterorderAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Ordinal numbers to cells in ascending order
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_order.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_order.html">PCRaster documentation</a>
 
             Parameters:
 
