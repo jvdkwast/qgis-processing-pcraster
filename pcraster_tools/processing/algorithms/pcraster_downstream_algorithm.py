d8368da771b795c8d44ffb63abb5f23f6a01d064
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 17:58:34 +0100

    Update pcraster_downstream_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_downstream_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_downstream_algorithm.py
@@ -46,7 +46,7 @@ class PCRasterDownstreamAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Cell gets value of the neighbouring downstream cell
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_downstream.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_downstream.html">PCRaster documentation</a>
 
             Parameters:
 
