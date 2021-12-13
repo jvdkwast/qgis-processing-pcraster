9e1e83de236b8766419aa7cd63bbd0ebfd4b966e
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:05:07 +0100

    Update pcraster_mapminimum_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_mapminimum_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_mapminimum_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterMapminimumAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Minimum cell value
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_mapminimum.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_mapminimum.html">PCRaster documentation</a>
 
             Parameters:
 
