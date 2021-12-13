540c47ad327663928b6c3dd64891742c8adc98ba
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 17:56:28 +0100

    Update pcraster_clump_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_clump_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_clump_algorithm.py
@@ -47,7 +47,7 @@ class PCRasterClumpAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Contiguous groups of cells with the same value (‘clumps’)
 
-                <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_clump.html">PCRaster documentation</a>
+                <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_clump.html">PCRaster documentation</a>
 
                 Parameters:
 
