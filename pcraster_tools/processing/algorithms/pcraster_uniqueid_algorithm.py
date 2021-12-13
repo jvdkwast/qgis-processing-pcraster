69be1576a05caebbf0a06aec79ec0134891d1af0
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:15:36 +0100

    Update pcraster_uniqueid_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_uniqueid_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_uniqueid_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterUniqueidAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Unique whole value for each Boolean TRUE cell
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_uniqueid.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_uniqueid.html">PCRaster documentation</a>
 
             Parameters:
 
