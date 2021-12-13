7c56da9f5e700f10ff1d6e7071294ed2ae0b2dcf
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 17:59:53 +0100

    Update pcraster_fac_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_fac_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_fac_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterfacAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Faculty or factorial of a natural positive number
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_fac.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_fac.html">PCRaster documentation</a>
 
             Parameters:
 
