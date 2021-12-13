7dacf700d283a546906da5b9c4c6ba89a8f40fdb
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 17:54:03 +0100

    Update pcraster_atan_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_atan_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_atan_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterAtanAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Inverse tangent
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_atan.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_atan.html">PCRaster documentation</a>
 
             Parameters:
 
