fef1b250a71fdfa7971996e9e57eeb0e27513f83
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 17:52:50 +0100

    Update pcraster_areatotal_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_areatotal_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_areatotal_algorithm.py
@@ -46,7 +46,7 @@ class PCRasterAreatotalAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Sum of cell values within an area
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_areatotal.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_areatotal.html">PCRaster documentation</a>
 
             Parameters:
 
