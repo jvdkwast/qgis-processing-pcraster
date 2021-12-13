0ae6fd0e188939102cf480546f72e5da43540cea
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:04:28 +0100

    Update pcraster_maparea_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_maparea_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_maparea_algorithm.py
@@ -47,7 +47,7 @@ class PCRasterMapareaAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Total map area
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_maparea.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_maparea.html">PCRaster documentation</a>
 
             Parameters:
 
