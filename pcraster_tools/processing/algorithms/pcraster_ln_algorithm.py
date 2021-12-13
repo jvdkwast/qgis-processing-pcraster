493b39ac5d70facb62579bd76bd86d5cceb87505
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:03:07 +0100

    Update pcraster_ln_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_ln_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_ln_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterlnAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Natural logarithm (e)
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_ln.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_ln.html">PCRaster documentation</a>
 
             Parameters:
 
