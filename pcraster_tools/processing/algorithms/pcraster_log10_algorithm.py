b221be89102ab1bed7c0ed6bbfd581923cb1a7e6
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:03:30 +0100

    Update pcraster_log10_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_log10_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_log10_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterlog10Algorithm(PCRasterAlgorithm):
         return self.tr(
             """Log 10
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_log10.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_log10.html">PCRaster documentation</a>
 
             Parameters:
 
