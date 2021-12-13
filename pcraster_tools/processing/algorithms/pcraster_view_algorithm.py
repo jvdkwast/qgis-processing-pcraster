ed2c90262f3b439df6ff89da7d64f16b051c1a47
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:16:15 +0100

    Update pcraster_view_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_view_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_view_algorithm.py
@@ -46,7 +46,7 @@ class PCRasterViewAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """TRUE or FALSE value for visibility from viewpoint(s) defined by a digital elevation model
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_view.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_view.html">PCRaster documentation</a>
 
             Parameters:
 
