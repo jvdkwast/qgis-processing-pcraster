c30c1a3e82d56e3fd349603d5cd8926ee61c7fc3
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:01:50 +0100

    Update pcraster_lddcreatedem_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_lddcreatedem_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_lddcreatedem_algorithm.py
@@ -54,7 +54,7 @@ class PCRasterLDDCreateDEMAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Modified digital elevation model
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_lddcreatedem.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_lddcreatedem.html">PCRaster documentation</a>
 
             Parameters:
 
