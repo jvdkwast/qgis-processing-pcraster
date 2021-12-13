6c2588ed2c9f1ea70d78bf750a2cf3b18805488d
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:13:32 +0100

    Update pcraster_streamorder_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_streamorder_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_streamorder_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterStreamOrderAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Stream order index of all cells on a local drain direction network
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_streamorder.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_streamorder.html">PCRaster documentation</a>
 
             Parameters:
 
