47016f5ebef96f62bc725a84fca8610180183a8f
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:02:06 +0100

    Update pcraster_ldddist_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_ldddist_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_ldddist_algorithm.py
@@ -49,7 +49,7 @@ class PCRasterLDDDistAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Friction-distance from the cell under consideration to downstream nearest TRUE cell
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_ldddist.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_ldddist.html">PCRaster documentation</a>
 
             Parameters:
 
