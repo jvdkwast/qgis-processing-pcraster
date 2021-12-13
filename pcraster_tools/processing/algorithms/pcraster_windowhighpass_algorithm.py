b649dd08b76b7b249a07951e2ae52d989a48d766
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:17:29 +0100

    Update pcraster_windowhighpass_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_windowhighpass_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_windowhighpass_algorithm.py
@@ -49,7 +49,7 @@ class PCRasterWindowHighPassAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Increases spatial frequency within a specified square neighbourhood
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_windowhighpass.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_windowhighpass.html">PCRaster documentation</a>
 
             Parameters:
 
