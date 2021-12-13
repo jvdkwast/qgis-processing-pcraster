8a5dec9968af4683ac5a029b927015a664713fbb
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:08:05 +0100

    Update pcraster_plancurv_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_plancurv_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_plancurv_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterPlancurvAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Planform curvature calculation using a DEM
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_plancurv.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_plancurv.html">PCRaster documentation</a>
 
             Parameters:
 
