a4bd219b692f3c23071c6e0592800d5eb0da3bce
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:13:14 +0100

    Update pcraster_sqrt_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_sqrt_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_sqrt_algorithm.py
@@ -45,7 +45,7 @@ class PCRastersqrtAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Square root
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_sqrt.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_sqrt.html">PCRaster documentation</a>
 
             Parameters:
 
