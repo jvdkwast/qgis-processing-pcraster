e2445a2aca54c022924d386b9f62707b8808275d
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:05:56 +0100

    Update pcraster_maptotal_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_maptotal_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_maptotal_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterMaptotalAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Sum of all cell values
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_maptotal.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_maptotal.html">PCRaster documentation</a>
 
             Parameters:
 
