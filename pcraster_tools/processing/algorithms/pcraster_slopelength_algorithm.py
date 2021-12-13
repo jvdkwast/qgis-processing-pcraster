85b03ed8daf77696d99746acbbd09a773d8f3083
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:10:33 +0100

    Update pcraster_slopelength_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_slopelength_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_slopelength_algorithm.py
@@ -48,7 +48,7 @@ class PCRasterSlopelengthAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Accumulative-friction-distance of the longest accumulative-friction-path upstream over the local drain direction network cells against waterbasin divides
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_slopelength.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_slopelength.html">PCRaster documentation</a>
 
             Parameters:
 
