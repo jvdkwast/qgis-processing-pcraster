dce185f9fec16bbd07da09df831155af5d1c483b
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:04:45 +0100

    Update pcraster_mapmaximum_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_mapmaximum_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_mapmaximum_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterMapmaximumAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Maximum cell value
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_mapmaximum.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_mapmaximum.html">PCRaster documentation</a>
 
             Parameters:
 
