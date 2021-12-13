e005d2bc3aed3af7cc19dcb1d4293f4143a8e821
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 17:57:40 +0100

    Update pcraster_cos_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_cos_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_cos_algorithm.py
@@ -45,7 +45,7 @@ class PCRastercosAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Cosine
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_cos.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_cos.html">PCRaster documentation</a>
 
             Parameters:
 
