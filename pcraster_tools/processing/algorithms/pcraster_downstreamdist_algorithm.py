fe1f2f162fac6c26b47aeea6fbc7ba757050a055
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 17:58:51 +0100

    Update pcraster_downstreamdist_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_downstreamdist_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_downstreamdist_algorithm.py
@@ -47,7 +47,7 @@ class PCRasterDownstreamdistAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Distance to the first cell downstream
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_downstreamdist.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_downstreamdist.html">PCRaster documentation</a>
 
             Parameters:
 
