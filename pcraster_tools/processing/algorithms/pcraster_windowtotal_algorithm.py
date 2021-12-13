60389b3398df03a43721018d0a36eee4193dfde3
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:18:46 +0100

    Update pcraster_windowtotal_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_windowtotal_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_windowtotal_algorithm.py
@@ -49,7 +49,7 @@ class PCRasterWindowTotalAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Sum of values within a specified square neighbourhood
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_windowtotal.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_windowtotal.html">PCRaster documentation</a>
 
             Parameters:
 
