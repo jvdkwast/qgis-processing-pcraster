ab3643711da4e3ce846a08cf05c995f5d9cf579e
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:14:09 +0100

    Update pcraster_succ_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_succ_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_succ_algorithm.py
@@ -45,7 +45,7 @@ class PCRastersuccAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Ordinal number of the next higher ordinal class
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_succ.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_succ.html">PCRaster documentation</a>
 
             Parameters:
 
