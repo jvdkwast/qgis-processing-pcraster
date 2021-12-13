712c56b5bfbd1895ee123ef8e6056ee38405f93c
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 17:53:22 +0100

    Update pcraster_asin_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_asin_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_asin_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterAsinAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Inverse sine
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_asin.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_asin.html">PCRaster documentation</a>
 
             Parameters:
 
