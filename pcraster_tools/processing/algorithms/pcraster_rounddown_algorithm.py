823f5f5ef691f6c1318265a4b49834bd085ec0f5
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:09:02 +0100

    Update pcraster_rounddown_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_rounddown_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_rounddown_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterrounddownAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Rounding down of cell values to whole numbers
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_rounddown.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_rounddown.html">PCRaster documentation</a>
 
             Parameters:
 
