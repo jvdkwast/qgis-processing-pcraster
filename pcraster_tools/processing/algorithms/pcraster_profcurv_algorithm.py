09f2aa25b3fab2a1628bce640de23de7ab2e98f3
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:08:41 +0100

    Update pcraster_profcurv_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_profcurv_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_profcurv_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterProfcurvAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Profile curvature calculation using a DEM
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_profcurv.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_profcurv.html">PCRaster documentation</a>
 
             Parameters:
 
