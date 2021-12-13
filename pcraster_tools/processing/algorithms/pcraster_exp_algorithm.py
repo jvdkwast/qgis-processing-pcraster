91eb8d3429d09ca5f505410f3e90fd9a6a663447
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 17:59:11 +0100

    Update pcraster_exp_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_exp_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_exp_algorithm.py
@@ -45,7 +45,7 @@ class PCRasterexpAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Base e exponential
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_exp.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_exp.html">PCRaster documentation</a>
 
             Parameters:
 
