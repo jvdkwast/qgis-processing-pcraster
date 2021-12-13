f459385f1631351dc74d83df553c876e7713aa24
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 17:55:37 +0100

    Update pcraster_cellarea_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_cellarea_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_cellarea_algorithm.py
@@ -47,7 +47,7 @@ class PCRastercellareaAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Area of one cell
 
-                <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_cellarea.html">PCRaster documentation</a>
+                <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_cellarea.html">PCRaster documentation</a>
 
                 Parameters:
 
