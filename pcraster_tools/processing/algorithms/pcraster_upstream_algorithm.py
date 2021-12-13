43f2b9d10f3ed483d70e26a7e4b3d4307025f476
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:15:54 +0100

    Update pcraster_upstream_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_upstream_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_upstream_algorithm.py
@@ -46,7 +46,7 @@ class PCRasterUpstreamAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Sum of the cell values of its first upstream cell(s)
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_upstream.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_upstream.html">PCRaster documentation</a>
 
             Parameters:
 
