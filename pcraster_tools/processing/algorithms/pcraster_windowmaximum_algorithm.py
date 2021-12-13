68d150433babed0f42a3aa0e6074dd1ed97a5d0d
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:18:05 +0100

    Update pcraster_windowmaximum_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_windowmaximum_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_windowmaximum_algorithm.py
@@ -49,7 +49,7 @@ class PCRasterWindowMaximumAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Maximum cell value within a specified square neighbourhood
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_windowmaximum.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_windowmaximum.html">PCRaster documentation</a>
 
             Parameters:
 
