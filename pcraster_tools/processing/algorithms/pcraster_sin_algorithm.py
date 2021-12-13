e6c6963065db0b89ec6b1fd8eaf716f09ecd0566
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:09:57 +0100

    Update pcraster_sin_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_sin_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_sin_algorithm.py
@@ -45,7 +45,7 @@ class PCRastersinAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Sine
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_sin.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_sin.html">PCRaster documentation</a>
 
             Parameters:
 
