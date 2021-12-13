71c693033acc1b7e1adfd001460b14766ada1784
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:01:27 +0100

    Update pcraster_lddcreate_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_lddcreate_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_lddcreate_algorithm.py
@@ -53,7 +53,7 @@ class PCRasterLDDCreateAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Local drain direction map with flow directions from each cell to its steepest downslope neighbour
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_lddcreate.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_lddcreate.html">PCRaster documentation</a>
 
             Parameters:
 
