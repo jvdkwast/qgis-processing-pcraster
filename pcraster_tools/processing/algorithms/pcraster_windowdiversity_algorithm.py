d828b9ea662ca5e91982a72a7092d77b30dd3f06
Hans van der Kwast <jvdkwast@yahoo.com>
2021-12-13 18:17:07 +0100

    Update pcraster_windowdiversity_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_windowdiversity_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_windowdiversity_algorithm.py
@@ -49,7 +49,7 @@ class PCRasterWindowDiversityAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Number of unique values within a specified square neighbourhood
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_windowdiversity.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.2/documentation/pcraster_manual/sphinx/op_windowdiversity.html">PCRaster documentation</a>
 
             Parameters:
 
