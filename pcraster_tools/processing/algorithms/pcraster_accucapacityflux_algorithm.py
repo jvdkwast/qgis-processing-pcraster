267c8d3e20f94b5dd967519f34085addd4b4a83b
Hans van der Kwast <jvdkwast@yahoo.com>
2022-01-11 17:45:15 +0100

    Update pcraster_accucapacityflux_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_accucapacityflux_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_accucapacityflux_algorithm.py
@@ -48,7 +48,7 @@ class PCRasterAccucapacityfluxAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Transport of material downstream over a local drain direction network
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_accucapacity.html">PCRaster documentation</a>
+            <a href="https://pcraster.geo.uu.nl/pcraster/latest/documentation/pcraster_manual/sphinx/op_accucapacity.html">PCRaster documentation</a>
 
             Parameters:
 
