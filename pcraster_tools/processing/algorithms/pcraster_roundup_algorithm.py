bbef61fb86e60dd7dae76cd8e876b2a8144bf868
Hans van der Kwast <jvdkwast@yahoo.com>
2022-02-07 18:18:46 +0100

    Update pcraster_roundup_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_roundup_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_roundup_algorithm.py
@@ -45,14 +45,14 @@ class PCRasterroundupAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Rounding up of cell values to whole numbers
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_roundup.html">PCRaster documentation</a>
+            <a href="{}">PCRaster documentation</a>
 
             Parameters:
 
             * <b>Input raster</b> (required) - scalar raster layer
             * <b>Output roundup raster</b> (required) - Scalar raster with result
             """
-        )
+        ).format(PCRasterAlgorithm.documentation_url('op_roundup.html'))
 
     def initAlgorithm(self, config=None):  # pylint: disable=missing-function-docstring,unused-argument
         self.addParameter(
