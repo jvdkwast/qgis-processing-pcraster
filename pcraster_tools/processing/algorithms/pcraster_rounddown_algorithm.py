8efd109206ee746f8ce68e39b825f01085fd7d52
Hans van der Kwast <jvdkwast@yahoo.com>
2022-02-07 18:17:26 +0100

    Update pcraster_rounddown_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_rounddown_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_rounddown_algorithm.py
@@ -45,14 +45,14 @@ class PCRasterrounddownAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Rounding down of cell values to whole numbers
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_rounddown.html">PCRaster documentation</a>
+            <a href="{}">PCRaster documentation</a>
 
             Parameters:
 
             * <b>Input raster</b> (required) - scalar raster layer
             * <b>Output rounddown raster</b> (required) - Scalar raster with result
             """
-        )
+        ).format(PCRasterAlgorithm.documentation_url('op_rounddown.html'))
 
     def initAlgorithm(self, config=None):  # pylint: disable=missing-function-docstring,unused-argument
         self.addParameter(
