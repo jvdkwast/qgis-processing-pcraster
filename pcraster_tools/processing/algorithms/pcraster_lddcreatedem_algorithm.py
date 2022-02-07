c1b5840800a079168ca05a4ab7049be8895f187f
Hans van der Kwast <jvdkwast@yahoo.com>
2022-02-07 18:02:06 +0100

    Update pcraster_lddcreatedem_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_lddcreatedem_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_lddcreatedem_algorithm.py
@@ -54,7 +54,7 @@ class PCRasterLDDCreateDEMAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Modified digital elevation model
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_lddcreatedem.html">PCRaster documentation</a>
+            <a href="{}">PCRaster documentation</a>
 
             Parameters:
 
@@ -68,7 +68,7 @@ class PCRasterLDDCreateDEMAlgorithm(PCRasterAlgorithm):
             * <b>Catchment precipitation</b> (required) - catchment precipitation
             * <b>Local drain direction layer output</b> (required) - raster with local drain direction (ldd data type)
             """
-        )
+        ).format(PCRasterAlgorithm.documentation_url('op_lddcreatedem.html'))
 
     def initAlgorithm(self, config=None):  # pylint: disable=missing-function-docstring,unused-argument
         # We add the input DEM.
