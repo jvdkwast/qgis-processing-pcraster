c73ecfcf35744b6616257d2702183e576080576f
Hans van der Kwast <jvdkwast@yahoo.com>
2022-02-07 17:41:56 +0100

    Update pcraster_atan_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_atan_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_atan_algorithm.py
@@ -45,14 +45,14 @@ class PCRasterAtanAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Inverse tangent
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_atan.html">PCRaster documentation</a>
+            <a href="{}">PCRaster documentation</a>
 
             Parameters:
 
             * <b>Input raster</b> (required) - scalar raster layer
             * <b>Output raster</b> (required) - Scalar raster with result
             """
-        )
+        ).format(PCRasterAlgorithm.documentation_url('op_atan.html'))
 
     def initAlgorithm(self, config=None):  # pylint: disable=missing-function-docstring,unused-argument
         self.addParameter(
