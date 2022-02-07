202a73582be8eab4843ceab6862c4b31cb428819
Hans van der Kwast <jvdkwast@yahoo.com>
2022-02-07 17:49:43 +0100

    Update pcraster_clump_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_clump_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_clump_algorithm.py
@@ -47,7 +47,7 @@ class PCRasterClumpAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Contiguous groups of cells with the same value (‘clumps’)
 
-                <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_clump.html">PCRaster documentation</a>
+                <a href="{}">PCRaster documentation</a>
 
                 Parameters:
 
@@ -55,7 +55,7 @@ class PCRasterClumpAlgorithm(PCRasterAlgorithm):
                  * <b>Input directions</b> (required) - diagonal (D8) or non-diagonal (D4)
                  * <b>Output clump raster layer</b> (required) - nominal raster layer with clumps
             """
-        )
+        ).format(PCRasterAlgorithm.documentation_url('op_clump.html'))
 
     def initAlgorithm(self, config=None):  # pylint: disable=missing-function-docstring,unused-argument
         self.addParameter(
