d1405c804f1df9f1b04cdff58027bb25782a5ba4
Hans van der Kwast <jvdkwast@yahoo.com>
2022-02-07 18:11:30 +0100

    Update pcraster_mapuniform_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_mapuniform_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_mapuniform_algorithm.py
@@ -45,14 +45,14 @@ class PCRasterMapuniformAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Cells get non spatial value taken from an uniform distribution
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_mapuniform.html">PCRaster documentation</a>
+            <a href="{}">PCRaster documentation</a>
 
             Parameters:
 
             * <b>Input mask raster layer</b> (required) - Raster layer of any data type with the mask for which the values will be calculated
             * <b>Output uniform raster</b> (required) - scalar raster layer with value assigned from a uniform distribution
             """
-        )
+        ).format(PCRasterAlgorithm.documentation_url('op_mapuniform.html'))
 
     def initAlgorithm(self, config=None):  # pylint: disable=missing-function-docstring,unused-argument
         self.addParameter(
