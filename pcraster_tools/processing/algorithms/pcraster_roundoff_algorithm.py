8e095c6c7e01eafd4224ee36f5f3f283ad398e77
Hans van der Kwast <jvdkwast@yahoo.com>
2022-02-07 18:18:08 +0100

    Update pcraster_roundoff_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_roundoff_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_roundoff_algorithm.py
@@ -45,14 +45,14 @@ class PCRasterroundoffAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Rounding off of cellvalues to whole numbers
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_roundoff.html">PCRaster documentation</a>
+            <a href="{}">PCRaster documentation</a>
 
             Parameters:
 
             * <b>Input raster</b> (required) - scalar raster layer
             * <b>Output roundoff raster</b> (required) - Scalar raster with result
             """
-        )
+        ).format(PCRasterAlgorithm.documentation_url('op_roundoff.html'))
 
     def initAlgorithm(self, config=None):  # pylint: disable=missing-function-docstring,unused-argument
         self.addParameter(
