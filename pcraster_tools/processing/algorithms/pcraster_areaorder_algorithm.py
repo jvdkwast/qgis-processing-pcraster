a7034d2cb3b969db50117d4b4bbc34e7afc1968a
Hans van der Kwast <jvdkwast@yahoo.com>
2022-02-07 17:34:41 +0100

    Update pcraster_areaorder_algorithm.py

--- pcraster_tools/processing/algorithms/pcraster_areaorder_algorithm.py
+++ pcraster_tools/processing/algorithms/pcraster_areaorder_algorithm.py
@@ -46,7 +46,7 @@ class PCRasterAreaorderAlgorithm(PCRasterAlgorithm):
         return self.tr(
             """Within each area ordinal numbers to cells in ascending order
 
-            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_areaorder.html">PCRaster documentation</a>
+            <a href="{}">PCRaster documentation</a>
 
             Parameters:
 
@@ -54,7 +54,7 @@ class PCRasterAreaorderAlgorithm(PCRasterAlgorithm):
             * <b>Input ordinal or scalar raster layer</b> ( required) - ordinal or scalar raster layer
             * <b>Output area normal raster</b> (required) - scalar raster layer order of cells in each class
             """
-        )
+        ).format(PCRasterAlgorithm.documentation_url('op_areaorder'))
 
     def initAlgorithm(self, config=None):  # pylint: disable=missing-function-docstring,unused-argument
         self.addParameter(
