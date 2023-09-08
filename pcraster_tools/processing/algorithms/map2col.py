b5536f7fae48b3f0fd6759074c7367570dc11ffc
Hans van der Kwast <jvdkwast@yahoo.com>
2023-09-08 09:59:40 +0200

    Raise exception if not .map

--- pcraster_tools/processing/algorithms/map2col.py
+++ pcraster_tools/processing/algorithms/map2col.py
@@ -16,6 +16,7 @@ import os
 from qgis.core import (QgsProcessing,
                        QgsProcessingParameterMultipleLayers,
                        QgsProcessingParameterFileDestination,
+                       QgsProcessingException
                        )
 
 from pcraster_tools.processing.algorithm import PCRasterAlgorithm
@@ -79,7 +80,9 @@ class Map2colAlgorithm(PCRasterAlgorithm):
     def processAlgorithm(self, parameters, context, feedback):  # pylint: disable=missing-function-docstring,unused-argument,too-many-locals
         input_rasters = []
         for layer in self.parameterAsLayerList(parameters, self.INPUT_RASTERS, context):
-            input_rasters.append(layer.source())
+          if not layer.source().lower().endswith('.map'):
+            raise QgsProcessingException('Only PCRaster .map format can be used as input')
+          input_rasters.append(layer.source())
 
         dst_filename = self.parameterAsFileOutput(parameters, self.OUTPUT_CSV, context)
         rasterstrings = ' '.join(f'"{raster}"' for raster in input_rasters)
